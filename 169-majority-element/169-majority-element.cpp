4e9719bb73bb086ffcd785956f4b7a8e592793e4
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-04-16 09:25:58 +0530

    Time: 20 ms (78.54%), Space: 19.5 MB (97.24%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int>mp;
+            for(int i=0;i<nums.size();i++)
+            {
+                mp[nums[i]]++;
+            }
+        int temp=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp[nums[i]]>nums.size()/2)
+            {return nums[i];
+            }
+        }
+        return 0;
+      
+        
+    }
+};

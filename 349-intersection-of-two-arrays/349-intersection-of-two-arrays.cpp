cac69de88a66fb78085b0369f2987d1ca210a4e3
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-04-18 09:59:49 +0530

    Time: 3 ms (95.33%), Space: 10.7 MB (24.59%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int>s1;
+        vector<int>res;
+        int c=0;
+        for(int i=0;i<nums1.size();i++)
+        {
+            s1.insert(nums1[i]);
+        }
+        set<int>s2;
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(s1.find(nums2[i])!=s1.end())
+            { s2.insert(nums2[i]);
+            }
+          
+            // res.push_back(nums2[i]);
+        }
+        for_each(  s2.begin(),
+                s2.end(),
+                [&](const auto & elem){
+                    res.push_back(elem);
+                });
+        return res;
+        
+    }
+};

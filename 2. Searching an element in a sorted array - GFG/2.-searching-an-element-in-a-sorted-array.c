d37083a10a406e3c4d6068b6f1c0e7f383f11b81
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-04-18 20:09:10 +0530

    Added solution - LeetHub

---
+++ 2. Searching an element in a sorted array - GFG/2.-searching-an-element-in-a-sorted-array.c
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h> 
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int searchInSorted(int arr[], int N, int K) 
+{ int low=0,high=N-1,mid=0;
+while(low<=high)
+{
+    mid=(low+high)/2;
+    if(arr[mid]==K)
+    return 1;
+    else if(arr[mid]<K)
+    low=mid+1;
+    else
+    high=mid-1;
+}
+return -1;
+       
+}
+
+// { Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n, k;
+        scanf("%d%d", &n, &k);
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            scanf("%d", &arr[i]);
+        }
+        
+        printf("%d\n", searchInSorted(arr, n, k));
+
+    }
+
+	return 0; 
+} 
+  // } Driver Code Ends

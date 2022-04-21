739f9015a4cec4864bbed5f8d50876d6e36d26b1
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-04-21 09:51:30 +0530

    Added solution - LeetHub

--- 5. Transpose of Matrix - GFG/5.-transpose-of-matrix.cpp
+++ 5. Transpose of Matrix - GFG/5.-transpose-of-matrix.cpp
@@ -26,9 +26,9 @@ class Solution
     // }
     for(int i=0; i<n; i++)
  {
-  for(int j=i; j<n; j++)
+  for(int j=i+1; j<n; j++)
   {
-    if(i!=j)  swap(matrix[i][j],matrix[j][i]);
+  swap(matrix[i][j],matrix[j][i]);
   }
   }
     }

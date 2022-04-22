23cc3b910aba7f2b5ce0f7c6f3c1337356619d8a
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-04-22 07:54:42 +0530

    Added solution - LeetHub

--- 6. Rotate by 90 degree - GFG/6.-rotate-by-90-degree.cpp
+++ 6. Rotate by 90 degree - GFG/6.-rotate-by-90-degree.cpp
@@ -14,7 +14,7 @@ class Solution
     
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i;j<n;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }

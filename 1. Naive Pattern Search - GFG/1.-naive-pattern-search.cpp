233d1a46ac1a8ee23c7b520e90059598cb972167
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-04-24 21:55:38 +0530

    Added solution - LeetHub

---
+++ 1. Naive Pattern Search - GFG/1.-naive-pattern-search.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// C++ program for Naive Pattern 
+// Searching algorithm 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if the given pattern exists in the given string or not.
+    bool search(string pat, string txt) 
+    {  int j = 0;
+    	for(int i  = 0; i< txt.length(); i++)
+    	{
+    	    if(txt[i] == pat[j])
+    	    {
+    	        j++;
+    	    }
+    	    else{
+    	        j = 0;
+    	    }
+    	    if(j == pat.length())
+    	        return true;
+    	}
+    	return false;
+    	
+    	
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver Code 
+int main() 
+{ 
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    string s, p;
+	    cin >> s >> p;
+	    Solution obj;
+	    if(obj.search(p, s)) cout << "Yes"; else cout << "No"; cout << endl;
+	}
+	
+	return 0; 
+}
+  // } Driver Code Ends

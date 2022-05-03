ac924115474a88f6dd5c8fb257b307c7cd412fd0
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-05-03 10:14:05 +0530

    Added solution - LeetHub

---
+++ 6. Stack using two queues - GFG/6.-stack-using-two-queues.cpp
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        QueueStack *qs = new QueueStack();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            qs->push(a);
+        }else if(QueryType==2){
+            cout<<qs->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/* The structure of the class is
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+ */
+
+//Function to push an element into stack using two queues.
+void QueueStack :: push(int x)
+{
+q1.push(x);
+}
+
+//Function to pop an element from stack using two queues. 
+int QueueStack :: pop()
+{
+  q2=q1;
+//   while(!q1.empty())
+// {
+//     q1.pop();
+// }
+// while(!q2.empty())
+// {q1.push(q2.front());
+// q2.pop();
+  
+// }
+// if(!q1.empty())
+// return q1.front();
+// return -1;// Your Code  
+if(q1.empty())
+return -1;
+int temp=q1.back();
+int n=q1.size();
+int i=0;
+while(i<n-1)
+{
+    q1.push(q1.front());
+    q1.pop();
+    i++;
+}
+q1.pop();
+return temp;
+
+
+}

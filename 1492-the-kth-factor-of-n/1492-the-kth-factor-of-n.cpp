a04c3c0309e0e251de8b77470b0eb04e912f4a6e
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-06-04 00:46:53 +0530

    Time: 4 ms (28.62%), Space: 6 MB (57.67%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        int a[1001]={0};
-        int j=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(n%i==0)
-            {
-                a[j++]=i;
-            }
+//         int a[1001]={0};
+//         int j=0;
+//         for(int i=1;i<=n;i++)
+//         {
+//             if(n%i==0)
+//             {
+//                 a[j++]=i;
+//             }
             
             
-        }
-        int c=0;
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]!=0)
-            {
-                c++;
+//         }
+//         int c=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(a[i]!=0)
+//             {
+//                 c++;
+//             }
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             cout<<a[i]<<" ";
+//         }
+//         cout<<endl;
+//         if(c<k)
+//         {
+//             return -1;
+//         }
+//         else
+//             return a[k-1];
+         for(int i=1;i<=n;i++){
+            if(n%i==0){
+                k--;
+                if(k==0)
+                    return i;
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            cout<<a[i]<<" ";
-        }
-        cout<<endl;
-        if(c<k)
-        {
-            return -1;
-        }
-        else
-            return a[k-1];
+        return -1;
         
         
     }

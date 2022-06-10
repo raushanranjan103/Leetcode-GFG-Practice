cfa73f50060a919f4ba0eed6f46a681e2c8730db
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-06-10 07:32:23 +0530

    Time: 57 ms (20.52%), Space: 7.9 MB (81.40%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) { 
-        int max_len = 0, l = 0, r = 0, size = s.length();
+        int len = 0, l = 0, r = 0, size = s.length();
         deque<char> seen;
         while (r < size)
         {
-            if (find(seen.begin(), seen.end(), s[r]) == seen.end()) {
-                seen.push_back(s[r]);
-                max_len = max(max_len, r - l + 1);
-                r++;
-            }
-            else {
-                seen.pop_front();
+            if (find(seen.begin(), seen.end(), s[r])!=seen.end()) {
+               seen.pop_front();
                 l++;
             }
+            else {
+               
+                 seen.push_back(s[r]);
+                len = max(len, r - l + 1);
+                r++;
+            }
         }
-        return max_len;
+        return len;
         
     }
 };

3f8e78d386eee228c7d1295a00ecdc995f834db9
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-06-15 19:01:47 +0530

    Time: 177 ms (9.82%), Space: 34.1 MB (16.37%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-          multimap<int,int>m;
-        vector<int>ans;
+//           multimap<int,int>m;
+         vector<int>ans;
         
+//         for(int i=0;i<arr.size();i++)
+//         {
+//             int temp=arr[i]-x;
+//             if(temp<0)
+//                 temp=-temp;
+            
+//             m.insert({temp,i});
+//         }
+        
+//         auto id=m.begin();
+        
+//         while(k>0)
+//         {
+//             ans.push_back(arr[id->second]);
+//             ++id;
+//             --k;
+   //     while()
+//         }
+        
+        
+//         sort(ans.begin(),ans.end());
+        
+//         return ans;
+        priority_queue<pair<int,int>>pq;
         for(int i=0;i<arr.size();i++)
         {
-            int temp=arr[i]-x;
-            if(temp<0)
-                temp=-temp;
-            
-            m.insert({temp,i});
+            pq.push({abs(arr[i]-x),arr[i]});
+            if(pq.size()>k)
+                pq.pop();
         }
-        
-        auto id=m.begin();
-        
-        while(k>0)
+        while(pq.size()>0)
         {
-            ans.push_back(arr[id->second]);
-            ++id;
-            --k;
+            ans.push_back(pq.top().second);
+            
+            pq.pop();
         }
-        
-        
-        sort(ans.begin(),ans.end());
-        
-        return ans;
-    
+     sort(ans.begin(),ans.end());
+    return ans;
     }
 };

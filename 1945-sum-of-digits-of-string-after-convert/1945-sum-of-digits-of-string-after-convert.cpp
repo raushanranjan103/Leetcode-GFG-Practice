8925ddab95ef1392cfc595f5e898dbc9cefcec11
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-06-24 22:18:43 +0530

    Time: 8 ms (24.53%), Space: 6.6 MB (16.67%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        int sum=0;
+         string t="";
+        for(int i=0;i<s.size();i++)
+        {
+            t+=to_string(((s[i]-'a')+1));
+        }
+        cout<<t<<endl;
+   stringstream ss; 
+   
+
+   ss <<t;
+        int x=0;
+   
+   // place the converted value to the int variable
+   ss >>x;
+   
+   //print to the consloe
+
+        int ans=0;
+        while(k--){
+            for(int i=0;i<t.length();i++){
+                sum += int(t[i]-'0');
+                ans = sum;
+            }
+            t.erase();
+            t= t+ to_string(sum);
+            sum=0;
+        }
+        return ans;
+    }
+};

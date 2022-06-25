6de7eb75452c844761ee142fbdc0637ae2f267d4
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-06-25 22:19:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (33.33%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   int countAsterisks(string s) {
+        bool flag=false;
+        int ans=0;
+        for(int i=0; i< s.length(); i++){
+            if(s[i]=='|')
+                flag=!flag;
+            else if(!flag && s[i]=='*')
+                ans++;            
+        }
+        return ans;
+        
+    }
+};

4805102e454808ea2de866ae453833aea6878a81
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-07-06 18:21:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-          if(n <= 1) return n;
-        return fib(n-1)+fib(n-2);
+                 if(n==0|n==1)
+                     return n;
+        int fibb[n+1];
+          fibb[0]=0,fibb[1]=1;
+        for(int i=2;i<=n;i++)
+        {fibb[i]=fibb[i-1]+fibb[i-2];
+            
+        }
+        return fibb[n];
     }
 };
